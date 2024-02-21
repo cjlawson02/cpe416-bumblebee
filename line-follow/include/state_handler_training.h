c7b3e4134dc0ea49ce84f1d9b20e15ed5c8f7937
Chris Lawson <git@chrislawson.dev>
2024-02-20 19:29:44 -0800

    feat: non blocking training

--- line-follow/include/state_handler_training.h
+++ line-follow/include/state_handler_training.h
@@ -7,7 +7,7 @@
 class TrainingMode : public IRobotState
 {
 public:
-    TrainingMode(NeuralNetwork *nn,NeuralNetwork *temp_nn, std::vector<struct TrainingData> *data_pts, float alpha);
+    TrainingMode(NeuralNetwork *nn, NeuralNetwork *temp_nn, std::vector<struct TrainingData> *data_pts, float alpha);
     ~TrainingMode();
 
     void init() override;
@@ -18,4 +18,7 @@ private:
     NeuralNetwork *m_neural, *m_temp_neural;
     std::vector<struct TrainingData> *m_data;
     float m_alpha;
+    unsigned long m_startTime;
+    size_t m_epoch;
+    size_t m_j;
 };
--- line-follow/src/state_handler_training.cpp
+++ line-follow/src/state_handler_training.cpp
@@ -2,13 +2,11 @@
 #include <time.h>
 #include <util/delay.h>
 
-TrainingMode::TrainingMode(NeuralNetwork *nn,NeuralNetwork *temp_nn, std::vector<struct TrainingData> *data_pts, float alpha)
- : m_neural(nn), m_temp_neural(temp_nn), m_data(data_pts), m_alpha(alpha)
+TrainingMode::TrainingMode(NeuralNetwork *nn, NeuralNetwork *temp_nn, std::vector<struct TrainingData> *data_pts, float alpha)
+    : m_neural(nn), m_temp_neural(temp_nn), m_data(data_pts), m_alpha(alpha), m_startTime(0), m_epoch(0), m_j(0)
 {
     m_stateName = TRAINING_MODE;
 }
-
-    
 
 TrainingMode::~TrainingMode()
 {
@@ -16,55 +14,57 @@ TrainingMode::~TrainingMode()
 
 void TrainingMode::init()
 {
-
+    // Print out the training screen
     clear_screen();
     print_string("Training");
-    
+
     lcd_cursor(0, 1);
     print_string("Pts: ");
     lcd_cursor(5, 1);
     print_num((u16)m_data->size());
 
-    _delay_ms(2000);
-
-    // clear_screen();
-
-
-    // print_string("Training");
-
-    //  for (size_t j = 0; j < m_data->size(); j++)
-    // {  
-    //     lcd_cursor(0, 1);
-    //     print_num((u16)(*m_data)[j].speeds.left_speed);
-    //     lcd_cursor(4, 1);
-    //     print_num((u16) (*m_data)[j].speeds.right_speed);
-    //     _delay_ms(1000);
-    // }
-
-    clear_screen();
-    print_string("Training");
-
-    for (size_t i = 0; i < 150; i++)
-    {
-        for (size_t j = 0; j < m_data->size()-1; j++)
-        {
-           m_neural->train((*m_data)[j],m_temp_neural,m_alpha);
-        }
-        clear_screen();
-        print_string("Training");
-        lcd_cursor(0, 1);
-        print_num((u16)i);
-        _delay_ms(10);
-    }
-
-    lcd_cursor(0, 1);
-    print_string("Done");
+    m_startTime = millis();
 }
 
 void TrainingMode::periodic()
 {
+    // Show the training screen for 2 seconds
+    if (millis() - m_startTime < 2000)
+    {
+        return;
+    }
+
+    // Train the neural network
+    if (m_epoch < 150)
+    {
+        m_neural->train((*m_data)[m_j], m_temp_neural, m_alpha);
+
+        // Move to the next data point
+        if (m_j < m_data->size() - 1)
+        {
+            m_j++;
+        }
+
+        // Move to the next epoch
+        else
+        {
+            m_j = 0;
+            m_epoch++;
+
+            clear_screen();
+            print_string("Training");
+            lcd_cursor(0, 1);
+            print_num((u16)m_epoch);
+            _delay_ms(10);
+        }
+    }
 }
 
 void TrainingMode::postPeriodic()
 {
+    // Training is done
+    clear_screen();
+    print_string("Training");
+    lcd_cursor(0, 1);
+    print_string("Done");
 }
